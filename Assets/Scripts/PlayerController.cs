4dc9adc1e7e0136ca7dcbe85b0501716037d45b0
mfurkansahn <76617516+mfurkansahn@users.noreply.github.com>
2024-05-07 12:15:07 +0300

    Bonus-Features-E

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -13,6 +13,13 @@ public class PlayerController : MonoBehaviour
     [SerializeField]
     float xRange = 10.0f;
 
+    //Easy: Bonus Features
+    [SerializeField]
+    float zRange = 10.0f;
+    [SerializeField]
+    float verticalInput;
+    //
+
     [SerializeField]
     GameObject projectilePrefab;
 
@@ -25,7 +32,7 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Oyuncunun sýnýrlarýný belirle
+        //Oyuncunun sýnýrlarýný belirle X eksenine göre
         if(transform.position.x < -xRange)
         {
             transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
@@ -36,10 +43,28 @@ public class PlayerController : MonoBehaviour
             transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
         }
 
+        //Easy: Bonus Features
+        //Oyuncunun sýnýrlarýný belirle Z eksenine göre
+        if (transform.position.z < (-zRange / 10)) //-1
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, (-zRange / 10));
+        }
+
+        if (transform.position.z > zRange)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zRange);
+        }
+        //
+
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * Time.deltaTime * speed * horizontalInput);
 
-        if(Input.GetKeyDown(KeyCode.Space))
+        //Easy: Bonus Features
+        verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
+        //
+
+        if (Input.GetKeyDown(KeyCode.Space))
         {
             //Launch a projectile from the player
             Instantiate(projectilePrefab, this.transform.position, projectilePrefab.transform.rotation);
